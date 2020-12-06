d88f796615d060c0710c66d1b007ba2167c0ef75
Kisha Prudente <kisha.prudente@gmail.com>
2020-12-06 18:26:16 -0500

    added projects data and images

---
+++ src/content/projects.js
@@ -0,0 +1,92 @@
+const projects = [
+  {
+    title: 'Chirrup!',
+    description:
+      'chirrup! provides a means for the user to log and share their emotions while also spreading positive encouragement to others.',
+    techStack: 'ReactJS, NodeJS, ExpressJS, MongoDB',
+    repo: 'https://github.com/kishaprudente/madagascar',
+    demo: 'https://chirrup-app.herokuapp.com/',
+    image: '/assets/images/projects/chirrup-mood.png',
+    date: 'July 2020',
+    // link: '/single-post',
+    icon: 'ti-light-bulb',
+  },
+  {
+    title: 'Eat Da Burgers!',
+    description: '',
+    techStack: 'JavaScript, HandlebarsJS, Bootstrap',
+    repo: 'https://github.com/kishaprudente/eat-da-burger',
+    demo: 'https://polar-dusk-74766.herokuapp.com/',
+    image: '/assets/images/projects/eat-da-burgers.png',
+    date: '2020',
+    // link: '/single-post',
+    icon: 'ti-light-bulb',
+  },
+  {
+    title: 'Employee Summary',
+    description: '',
+    techStack: 'JavaScript, React, NodeJS, Express',
+    repo: 'hhttps://github.com/kishaprudente/employee-directory-react',
+    demo: 'https://kp-react-employees.herokuapp.com/',
+    image: '/assets/images/projects/employee-directory.png',
+    date: '2020',
+    // link: '/single-post',
+    icon: 'ti-light-bulb',
+  },
+  {
+    title: 'Weather Dashboard',
+    description: '',
+    techStack: 'HTML, jQuery, Bootstrap',
+    repo: 'https://github.com/kishaprudente/weather-dashboard',
+    demo: 'https://kishaprudente.github.io/weather-dashboard/',
+    image: '/assets/images/projects/weather-dashboard.png',
+    date: '2020',
+    // link: '/single-post',
+    icon: 'ti-light-bulb',
+  },
+  {
+    title: 'Google Books Search',
+    description: '',
+    techStack: 'ReactJS, NodeJS, MongoDB, Express',
+    repo: 'https://github.com/kishaprudente/google-books-search',
+    demo: 'https://kp-google-books-search.herokuapp.com/',
+    image: '/assets/images/projects/google-books.png',
+    date: 'June 2020',
+    // link: '/single-post',
+    icon: 'ti-light-bulb',
+  },
+];
+
+export default projects;
+
+// const projetcs = [
+//   {
+//     title: 'Chirrup',
+//     image: '/assets/images/blog/5-1.jpg',
+//     description: '',
+//     date: 'May 15th, 2019',
+//     link: '/single-post',
+//     icon: 'ti-light-bulb',
+//   },
+//   {
+//     title: 'The Best Sale Marketer Of The Next Year',
+//     image: '/assets/images/blog/5-2.jpg',
+//     date: 'May 18th, 2019',
+//     link: '/single-post',
+//     icon: 'ti-mobile',
+//   },
+//   {
+//     title: 'What gets in the way of greate Stategy',
+//     image: '/assets/images/blog/5-3.jpg',
+//     date: 'May 21h, 2019',
+//     link: '/single-post',
+//     icon: 'ti-paint-bucket',
+//   },
+//   {
+//     title: 'How to create great logo for your Business',
+//     image: '/assets/images/blog/5-4.jpg',
+//     date: 'May 21th, 2019',
+//     link: '/single-post',
+//     icon: 'ti-light-bulb',
+//   },
+// ];
