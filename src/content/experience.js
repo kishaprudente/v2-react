c7b9cfb9d3cee54d90e4afbe2f95849ac64d0252
Kisha Prudente <kisha.prudente@gmail.com>
2020-12-05 23:40:35 -0500

    added description

--- src/content/experience.js
+++ src/content/experience.js
@@ -3,7 +3,11 @@ const experience = [
     title: 'Web & Mobile Developer',
     subtitle: 'BoomNation | Remote',
     description:
-      'Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo.',
+      'The largest database of skilled workers, tradesman, and truckers in the world. A community focused on finding jobs, upskilling, and reskilling',
+    // bullets: [
+    //   'Develop mobile app using Flutter',
+    //   ''
+    // ],
     date: '(2020-Present)',
     icon: 'ti-pencil-alt',
   },
@@ -17,7 +21,7 @@ const experience = [
       '- Provide emotional support to students strugling to understand coding fundamentals.',
       ' - Guide students to understand concepts of coding.',
     ],
-    date: '(2016-2018)',
+    date: '(2020-Present)',
     icon: 'ti-heart',
   },
 ];
