38eb50e87358928422c7338cf0e6034017aa11f6
Kisha Prudente <kisha.prudente@gmail.com>
2020-12-05 22:47:01 -0500

    added content to resume

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -2,87 +2,11 @@ import React from 'react';
 import { Container, Row, Col } from 'react-grid-system';
 // Components
 import ProgressBar from '../components/common/ProgressBar';
-
-const skills = [
-  {
-    title: 'JavaScript',
-    percent: 85,
-  },
-  {
-    title: 'Node.js',
-    percent: 85,
-  },
-  {
-    title: 'React.js',
-    percent: 90,
-  },
-  {
-    title: 'HTML',
-    percent: 90,
-  },
-  {
-    title: 'CSS',
-    percent: 70,
-  },
-  {
-    title: 'MongoDB',
-    percent: 75,
-  },
-];
-const education = [
-  {
-    title: 'Columbia University in the City of New York',
-    subtitle: 'Full Stack Web Development Coding Boot Camp | New York',
-    description:
-      'Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo.',
-    bullets: [],
-    date: '(2020-2020)',
-    icon: 'ti-crown',
-  },
-  {
-    title: 'Central Philippine University',
-    subtitle: 'Bachelor of Science in Software Engineering | Philippines',
-    description: `• Member of Philippine Society of Software Engineers `,
-    bullets: [
-      '• Coursework in:',
-      '- Software Development',
-      '- Enterprise Application Development',
-      '- Software Project Management',
-      '- Software Verification and Validation',
-      '- Software Engineering Processes',
-      '- Principles of Software Architecture',
-      '- Software Quality Assurance',
-      '- Operating Systems',
-      '- Software Configuration Management',
-      '- Computer Architecture',
-    ],
-    date: '(2013-2019)',
-    icon: 'ti-ruler-alt-2',
-  },
-];
-const experience = [
-  {
-    title: 'Web & Mobile Developer',
-    subtitle: 'BoomNation | Remote',
-    description:
-      'Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo.',
-    date: '(2020-Present)',
-    icon: 'ti-pencil-alt',
-  },
-  {
-    title: 'Coding Tutor',
-    subtitle: 'Trilogy Education | Remote',
-    description:
-      'One-on-one tutor offering remote, supplemental educational assistance to students in Trilogy educational bootcamps nationwide.',
-    bullets: [
-      '- Topics covered: JavaScript, NodeJS, ReactJS, ExpressJS, Bootstrap, HTML5, CSS3, Git, GitHub, SQL, NoSQL, Object Oriented Programing.',
-      '- Provide emotional support to students strugling to understand coding fundamentals.',
-      ' - Guide students to understand concepts of coding.',
-    ],
-    date: '(2016-2018)',
-    icon: 'ti-heart',
-  },
-];
+// content
+import education from '../content/education';
+import experience from '../content/experience';
+import skills from '../content/skills';
+import certificates from '../content/certificates';
 
 function Resume() {
   return (
@@ -164,15 +88,60 @@ function Resume() {
             </Col>
           </Row>
         </Container>
+        <header className='el-heading el-heading-center'>
+          <h3>Certificates & Skills</h3>
+          <div className='divider divider-1-reverse divider-1-1'></div>
+          <div className='divider divider-1-reverse divider-1-2'></div>
+        </header>
         <Container>
           <Row>
-            {skills.map((item, index) => (
-              <Col key={index} sm={6} xl={4}>
-                <div className='skills-item'>
-                  <ProgressBar label={item.title} percent={item.percent} />
+            <Col md={6}>
+              <div className='resume-row'>
+                <h4 className='resume-title'>Certificates</h4>
+                {/* <Row> */}
+                <ul>
+                  {certificates.map((item, index) => (
+                    <li key={index}>
+                      <Col>
+                        <div className='resume-item'>
+                          <div className='resume-head'>
+                            <div className='el-icon'>
+                              <span
+                                className={`el-icon-icon ${item.icon}`}
+                              ></span>
+                            </div>
+                            <h5>{item.title}</h5>
+                            <h6>
+                              {item.organization} <small>{item.date}</small>
+                            </h6>
+                          </div>
+                        </div>
+                      </Col>
+                    </li>
+                  ))}
+                </ul>
+                {/* </Row> */}
+              </div>
+            </Col>
+            <Col md={6}>
+              <div className='resume-row'>
+                <div className='mb-40'>
+                  <h4 className='resume-title'>Skills</h4>
                 </div>
-              </Col>
-            ))}
+                <Row>
+                  {skills.map((item, index) => (
+                    <Col key={index} md={6} lg={6}>
+                      <div className='skills-item'>
+                        <ProgressBar
+                          label={item.title}
+                          percent={item.percent}
+                        />
+                      </div>
+                    </Col>
+                  ))}
+                </Row>
+              </div>
+            </Col>
           </Row>
         </Container>
       </div>
