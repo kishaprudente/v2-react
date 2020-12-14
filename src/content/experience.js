603998f2328e5c026e3e77d1cf3ca650ff138f18
Kisha Prudente <kisha.prudente@gmail.com>
2020-12-13 23:31:46 -0500

    added experience description

--- src/content/experience.js
+++ src/content/experience.js
@@ -1,29 +1,31 @@
 const experience = [
-  {
-    title: 'Web & Mobile Developer',
-    subtitle: 'BoomNation | Remote',
-    description:
-      'The largest database of skilled workers, tradesman, and truckers in the world. A community focused on finding jobs, upskilling, and reskilling',
-    // bullets: [
-    //   'Develop mobile app using Flutter',
-    //   ''
-    // ],
-    date: '(2020-Present)',
-    icon: 'ti-pencil-alt',
-  },
-  {
-    title: 'Coding Tutor',
-    subtitle: 'Trilogy Education | Remote',
-    description:
-      'One-on-one tutor offering remote, supplemental educational assistance to students in Trilogy educational bootcamps nationwide.',
-    bullets: [
-      '- Topics covered: JavaScript, NodeJS, ReactJS, ExpressJS, Bootstrap, HTML5, CSS3, Git, GitHub, SQL, NoSQL, Object Oriented Programing.',
-      '- Provide emotional support to students strugling to understand coding fundamentals.',
-      ' - Guide students to understand concepts of coding.',
-    ],
-    date: '(2020-Present)',
-    icon: 'ti-heart',
-  },
+	{
+		title: 'Web & Mobile Developer',
+		subtitle: 'BoomNation | Remote',
+		description:
+			'The largest database of skilled workers, tradesman, and truckers in the world. A community focused on finding jobs, upskilling, and reskilling',
+		bullets: [
+			'- Technologies used: Flutter, GraphQL, Firebase, Angular, Typescript, Azure',
+			'- Effectively coded software changes and alterations based on specific design specifications.',
+			'- Brought forth a passion and dedication to software development.',
+			'- Collaborate with the team using SCRUM-based approach',
+		],
+		date: '(2020-Present)',
+		icon: 'ti-pencil-alt',
+	},
+	{
+		title: 'Coding Tutor',
+		subtitle: 'Trilogy Education | Remote',
+		description:
+			'One-on-one tutor offering remote, supplemental educational assistance to students in Trilogy educational bootcamps nationwide.',
+		bullets: [
+			'- Topics covered: JavaScript, NodeJS, ReactJS, ExpressJS, Bootstrap, HTML5, CSS3, Git, GitHub, SQL, NoSQL, Object Oriented Programing.',
+			'- Provide emotional support to students strugling to understand coding fundamentals.',
+			'- Guide students to understand concepts of coding.',
+		],
+		date: '(2020-Present)',
+		icon: 'ti-heart',
+	},
 ];
 
 export default experience;
