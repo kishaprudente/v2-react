72c7d8c97937c4907e84e3755ede7fee5644f940
Kisha Prudente <kisha.prudente@gmail.com>
2020-12-13 23:28:40 -0500

    add link to CV

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,91 +2,87 @@ import React from 'react';
 import { Container, Hidden, Row, Col } from 'react-grid-system';
 
 function About() {
-  const recentTech = [
-    'JavaScript',
-    'Flutter',
-    'Node.js',
-    'React.js',
-    'Gatsbyjs',
-    'Angular',
-  ];
-  return (
-    <section className='section section-about section-about-1'>
-      <div className='display-spacing'>
-        <Container>
-          <Row>
-            <Hidden xs sm md>
-              <Col lg={5}>
-                <div className='about-image'>
-                  <div
-                    className='image overlay-image'
-                    style={{
-                      backgroundImage: `url(/assets/images/kisha/me.jpg)`,
-                    }}
-                  ></div>
-                </div>
-              </Col>
-            </Hidden>
-            <Col lg={7}>
-              <div className='about-content'>
-                <header className='el-heading'>
-                  <h2 className='el-icon'>
-                    <span className='el-icon-title'>ABOUT ME</span>
-                  </h2>
-                  <h3>
-                    <span>Hello, I am Kisha Prudente.</span>
-                  </h3>
-                  <div className='divider divider-1'></div>
-                  <p>
-                    I am currently based in New York City working as a Web and
-                    Mobile Developer and Coding Tutor on the side. I was born
-                    and raised in the Philippines where I graduated with a
-                    Bachelor's degree in Software Engineering. I enjoy
-                    collaborating with individuals who loves coding as much as I
-                    do. My goal is to always build high quality software and web
-                    applications.
-                  </p>
-                  <p>
-                    I am highly self-motivated, always willing to improve and
-                    push the boundaries in any way possible. I keep myself up to
-                    date with recent technologies and concepts to solve
-                    problems.
-                  </p>
-                  <p>
-                    When I'm not coding, you can find me watching scary movies,
-                    traveling, and spending time with my husband and 2 year old
-                    daughter.
-                  </p>
-                </header>
-                <Row className='pt-30'>
-                  <Col xs={12} sm={12} md={12}>
-                    <div className='el-card-icon'>
-                      <div className='el-icon'>
-                        <span className='el-icon-icon ti-desktop'></span>
-                      </div>
-                      <div className='content'>
-                        <h5>Technologies I have been working with recently:</h5>
-                        <Row>
-                          {recentTech.map((item, index) => (
-                            <Col key={index} xs={6} sm={6} md={6}>
-                              <div className='pt-30'>
-                                <span className='el-icon-icon ti-angle-double-right'></span>{' '}
-                                {item}
-                              </div>
-                            </Col>
-                          ))}
-                        </Row>
-                      </div>
-                    </div>
-                  </Col>
-                </Row>
-              </div>
-            </Col>
-          </Row>
-        </Container>
-      </div>
-    </section>
-  );
+	const recentTech = ['JavaScript', 'Flutter', 'Node.js', 'React.js', 'Gatsbyjs', 'Angular'];
+	return (
+		<section className='section section-about section-about-1'>
+			<div className='display-spacing'>
+				<Container>
+					<Row>
+						<Hidden xs sm md>
+							<Col lg={5}>
+								<div className='about-image'>
+									<div
+										className='image overlay-image'
+										style={{
+											backgroundImage: `url(/assets/images/kisha/me.jpg)`,
+										}}
+									></div>
+								</div>
+							</Col>
+						</Hidden>
+						<Col lg={7}>
+							<div className='about-content'>
+								<header className='el-heading'>
+									<h2 className='el-icon'>
+										<span className='el-icon-title'>ABOUT ME</span>
+									</h2>
+									<h3>
+										<span>Hello, I am Kisha Prudente.</span>
+									</h3>
+									<div className='divider divider-1'></div>
+									<p>
+										I am currently based in New York City working as a Web and
+										Mobile Developer and Coding Tutor on the side. I was born
+										and raised in the Philippines where I graduated with a
+										Bachelor's degree in Software Engineering. I enjoy
+										collaborating with individuals who love coding as much as I
+										do. My goal is to always build high quality software and web
+										applications.
+									</p>
+									<p>
+										I am highly self-motivated, always willing to improve and
+										push the boundaries in any way possible. I keep myself up to
+										date with recent technologies and concepts to solve
+										problems. I appreciate chellenges as they are opportunities
+										to explore solutions.
+									</p>
+									<p>
+										When I'm not coding, you can find me watching scary movies,
+										traveling, and spending time with my husband and 2 year old
+										daughter.
+									</p>
+								</header>
+								<Row className='pt-30'>
+									<Col xs={12} sm={12} md={12}>
+										<div className='el-card-icon'>
+											<div className='el-icon'>
+												<span className='el-icon-icon ti-desktop'></span>
+											</div>
+											<div className='content'>
+												<h5>
+													Technologies I have been working with recently:
+												</h5>
+												<Row>
+													{recentTech.map((item, index) => (
+														<Col key={index} xs={6} sm={6} md={6}>
+															<div className='pt-30'>
+																<span className='el-icon-icon ti-angle-double-right'></span>{' '}
+																{item}
+															</div>
+														</Col>
+													))}
+												</Row>
+											</div>
+										</div>
+									</Col>
+								</Row>
+							</div>
+						</Col>
+					</Row>
+				</Container>
+			</div>
+		</section>
+	);
 }
 
 export default About;
--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -9,151 +9,152 @@ import skills from '../content/skills';
 import certificates from '../content/certificates';
 
 function Resume() {
-  return (
-    <section className='section section-resume section-resume-1'>
-      <div className='display-spacing'>
-        <header className='el-heading el-heading-center'>
-          <div className='el-icon'>
-            <h2 className='el-icon-title'>
-              <span>RESUME</span>
-            </h2>
-          </div>
-          <h3>Education & Experience</h3>
-          <div className='divider divider-1-reverse divider-1-1'></div>
-          <div className='divider divider-1-reverse divider-1-2'></div>
-          <div className='div-center text-center lets-work'>
-            <a target='_blank' href='/assets/images/kisha/Resume.pdf'>
-              <button type='button' className='button button-md button-primary'>
-                Download my CV
-              </button>
-            </a>
-          </div>
-        </header>
-        <Container className='mb-spacing'>
-          <Row>
-            <Col md={6}>
-              <div className='resume-row'>
-                <h4 className='resume-title'>Education</h4>
-                <ul>
-                  {education.map((item, index) => (
-                    <li key={index}>
-                      <div className='resume-item'>
-                        <div className='resume-head'>
-                          <div className='el-icon'>
-                            <span
-                              className={`el-icon-icon ${item.icon}`}
-                            ></span>
-                          </div>
-                          <h5>{item.title}</h5>
-                          <h6>{item.subtitle}</h6>
-                        </div>
-                        <div className='resume-body'>
-                          <p>{item.description}</p>
-                          {item.bullets
-                            ? item.bullets.map((bullet, index) => (
-                                <p key={index}>{bullet}</p>
-                              ))
-                            : null}
-                        </div>
-                      </div>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            </Col>
-            <Col md={6}>
-              <div className='resume-row'>
-                <h4 className='resume-title'>Experience</h4>
-                <ul>
-                  {experience.map((item, index) => (
-                    <li key={index}>
-                      <div className='resume-item'>
-                        <div className='resume-head'>
-                          <div className='el-icon'>
-                            <span
-                              className={`el-icon-icon ${item.icon}`}
-                            ></span>
-                          </div>
-                          <h5>{item.title}</h5>
-                          <h6>
-                            {item.subtitle} <small>{item.date}</small>
-                          </h6>
-                        </div>
-                        <div className='resume-body'>
-                          <p>{item.description}</p>
-                          {item.bullets
-                            ? item.bullets.map((bullet, index) => (
-                                <p key={index}>{bullet}</p>
-                              ))
-                            : null}
-                        </div>
-                      </div>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            </Col>
-          </Row>
-        </Container>
-        <header className='el-heading el-heading-center'>
-          <h3>Certificates & Skills</h3>
-          <div className='divider divider-1-reverse divider-1-1'></div>
-          <div className='divider divider-1-reverse divider-1-2'></div>
-        </header>
-        <Container>
-          <Row>
-            <Col md={6}>
-              <div className='resume-row'>
-                <h4 className='resume-title'>Certificates</h4>
-                {/* <Row> */}
-                <ul>
-                  {certificates.map((item, index) => (
-                    <li key={index}>
-                      <Col>
-                        <div className='resume-item'>
-                          <div className='resume-head'>
-                            <div className='el-icon'>
-                              <span
-                                className={`el-icon-icon ${item.icon}`}
-                              ></span>
-                            </div>
-                            <h5>{item.title}</h5>
-                            <h6>
-                              {item.organization} <small>{item.date}</small>
-                            </h6>
-                          </div>
-                        </div>
-                      </Col>
-                    </li>
-                  ))}
-                </ul>
-                {/* </Row> */}
-              </div>
-            </Col>
-            <Col md={6}>
-              <div className='resume-row'>
-                <div className='mb-40'>
-                  <h4 className='resume-title'>Skills</h4>
-                </div>
-                <Row>
-                  {skills.map((item, index) => (
-                    <Col key={index} md={6} lg={6}>
-                      <div className='skills-item'>
-                        <ProgressBar
-                          label={item.title}
-                          percent={item.percent}
-                        />
-                      </div>
-                    </Col>
-                  ))}
-                </Row>
-              </div>
-            </Col>
-          </Row>
-        </Container>
-      </div>
-    </section>
-  );
+	return (
+		<section className='section section-resume section-resume-1'>
+			<div className='display-spacing'>
+				<header className='el-heading el-heading-center'>
+					<div className='el-icon'>
+						<h2 className='el-icon-title'>
+							<span>RESUME</span>
+						</h2>
+					</div>
+					<h3>Education & Experience</h3>
+					<div className='divider divider-1-reverse divider-1-1'></div>
+					<div className='divider divider-1-reverse divider-1-2'></div>
+					<div className='div-center text-center lets-work'>
+						<a target='_blank' href='https://www.visualcv.com/kishaprudente/'>
+							<button type='button' className='button button-md button-primary'>
+								Download my CV
+							</button>
+						</a>
+					</div>
+				</header>
+				<Container className='mb-spacing'>
+					<Row>
+						<Col md={6}>
+							<div className='resume-row'>
+								<h4 className='resume-title'>Education</h4>
+								<ul>
+									{education.map((item, index) => (
+										<li key={index}>
+											<div className='resume-item'>
+												<div className='resume-head'>
+													<div className='el-icon'>
+														<span
+															className={`el-icon-icon ${item.icon}`}
+														></span>
+													</div>
+													<h5>{item.title}</h5>
+													<h6>{item.subtitle}</h6>
+												</div>
+												<div className='resume-body'>
+													<p>{item.description}</p>
+													{item.bullets
+														? item.bullets.map((bullet, index) => (
+																<p key={index}>{bullet}</p>
+														  ))
+														: null}
+												</div>
+											</div>
+										</li>
+									))}
+								</ul>
+							</div>
+						</Col>
+						<Col md={6}>
+							<div className='resume-row'>
+								<h4 className='resume-title'>Experience</h4>
+								<ul>
+									{experience.map((item, index) => (
+										<li key={index}>
+											<div className='resume-item'>
+												<div className='resume-head'>
+													<div className='el-icon'>
+														<span
+															className={`el-icon-icon ${item.icon}`}
+														></span>
+													</div>
+													<h5>{item.title}</h5>
+													<h6>
+														{item.subtitle} <small>{item.date}</small>
+													</h6>
+												</div>
+												<div className='resume-body'>
+													<p>{item.description}</p>
+													{item.bullets
+														? item.bullets.map((bullet, index) => (
+																<p key={index}>{bullet}</p>
+														  ))
+														: null}
+												</div>
+											</div>
+										</li>
+									))}
+								</ul>
+							</div>
+						</Col>
+					</Row>
+				</Container>
+				<header className='el-heading el-heading-center'>
+					<h3>Certificates & Skills</h3>
+					<div className='divider divider-1-reverse divider-1-1'></div>
+					<div className='divider divider-1-reverse divider-1-2'></div>
+				</header>
+				<Container>
+					<Row>
+						<Col md={6}>
+							<div className='resume-row'>
+								<h4 className='resume-title'>Certificates</h4>
+								{/* <Row> */}
+								<ul>
+									{certificates.map((item, index) => (
+										<li key={index}>
+											<Col>
+												<div className='resume-item'>
+													<div className='resume-head'>
+														<div className='el-icon'>
+															<span
+																className={`el-icon-icon ${item.icon}`}
+															></span>
+														</div>
+														<h5>{item.title}</h5>
+														<h6>
+															{item.organization}{' '}
+															<small>{item.date}</small>
+														</h6>
+													</div>
+												</div>
+											</Col>
+										</li>
+									))}
+								</ul>
+								{/* </Row> */}
+							</div>
+						</Col>
+						<Col md={6}>
+							<div className='resume-row'>
+								<div className='mb-40'>
+									<h4 className='resume-title'>Skills</h4>
+								</div>
+								<Row>
+									{skills.map((item, index) => (
+										<Col key={index} md={6} lg={6}>
+											<div className='skills-item'>
+												<ProgressBar
+													label={item.title}
+													percent={item.percent}
+												/>
+											</div>
+										</Col>
+									))}
+								</Row>
+							</div>
+						</Col>
+					</Row>
+				</Container>
+			</div>
+		</section>
+	);
 }
 
 export default Resume;
